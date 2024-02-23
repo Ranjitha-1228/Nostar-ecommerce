2ae6aa7dfa75e49663dfd46d24632b603bb5a65e
Ranjitha-1228 <ranjithamurugasan1228@gmail.com>
2024-02-23 11:49:30 +0530

    update

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let offer_bar = document.querySelector(".offer_bar")
-let offer = document.getElementById("offer_closed")
+var offer_bar = document.querySelector(".offer_bar")
+var offer = document.getElementById("offer_closed")
 offer.addEventListener("click",function(){
     offer_bar.style.display="none";
 })
 
-let sidenav = document.getElementById("side_nav")
-let menu = document.getElementById("menu")
-let close = document.getElementById("close")
+var sidenav = document.getElementById("side_nav")
+var menu = document.getElementById("menu")
+var close = document.getElementById("close")
 
 menu.addEventListener("click",function(){
     sidenav.style.left=0
@@ -15,12 +15,12 @@ close.addEventListener("click",function(){
     sidenav.style.left="-40%"
 })
 
-let product = document.getElementById("product")
-let search =  document.getElementById("search")
-let productlist = product.querySelectorAll("div")
+var product = document.getElementById("product")
+var search =  document.getElementById("search")
+var productlist = product.querySelectorAll("div")
 console.log(productlist)
 search.addEventListener("keyup",function(){
-    let entervalue = event.target.value.toUpperCase()
+    var entervalue = this.target.value.toUpperCase()
 
     for (count=0;count<productlist.length;count=count+1){
         var productname = productlist[count].querySelector("h1").textContent
